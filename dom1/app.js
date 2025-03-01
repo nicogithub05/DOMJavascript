5d4d6b70fcf133bff74fe685a9a3ab46f5fdb91f
nivamol <nicolasgithub@gmail.com>
2025-03-01 13:11:45 -0500

    Definiendo y accediendo a nodos

---
+++ dom1/app.js
@@ -0,0 +1,5 @@
+console.log('Hola Mundo!')
+console.dir(document); // Obtenemos la información del DOM. cómo está compuesto
+// Todo lo que existe en el DOM y que puedo manipular.
+
+
---
+++ dom2/selecting-dom-elements.js
@@ -0,0 +1,7 @@
+console.log(document.getElementById("app-title"));
+console.log(document.querySelector("#app-title"));
+console.log(document.querySelector("p")); // trae solo el primer elemento
+console.log(document.querySelectorAll("p"));
+
+console.log(document.getElementsByClassName("menu-items"));
+console.log(document.getElementsByTagName('p'));
