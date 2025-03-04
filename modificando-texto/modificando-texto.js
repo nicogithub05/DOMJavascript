26b6e9e588b93d2881984a3ed6a2b032c7c3603d
nivamol <nicolasgithub@gmail.com>
2025-03-04 18:50:35 -0500

    modificando texto en HTML

---
+++ modificando-texto/modificando-texto.js
@@ -0,0 +1,9 @@
+const title = document.querySelector("#app-title");
+
+title.textContent = "Nuevo Texto";
+
+console.log(title);
+
+const opcion2 = title.innerText = "Este es otro título";
+
+console.log(opcion2);
