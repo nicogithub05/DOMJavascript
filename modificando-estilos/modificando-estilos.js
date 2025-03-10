83e386589766ca8f13ea3452d10ccc52aa2e08f5
nivamol <nicolasgithub@gmail.com>
2025-03-10 15:33:31 -0500

    modificando estilos

---
+++ modificando-estilos/modificando-estilos.js
@@ -0,0 +1,21 @@
+const title = document.querySelector("h1");
+
+title.style // Etiqueta principal para agregar estilos
+
+title.style.color = "Red"; // agregamos (en línea en la etiqueta style) el estilo color
+
+______________
+
+const menu = document.querySelector("menu")
+
+menu.style.backgroundColor = "red"; // modificamos el background color
+
+______________
+
+menu.style.fontSize = "25px"; // cambiando el font-size
+_____________
+
+// acceder a la clase del elemento 
+
+menu.className = "main-menu" // accedemos al elemento con clase main-menu
+// remplaza la clase por main-menu.
